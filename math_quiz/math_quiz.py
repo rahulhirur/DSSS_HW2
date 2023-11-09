9394eb24a55a411a3ed350d5eebac92a28d25f1c
rahul-alpha <rahul.hirur@fau.de>
2023-11-09 12:07:04 +0100

    Bug fixes to the math_quiz.py files

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,95 @@
 import random
 
 
-def function_A(min, max):
+def generateInteger(min, max):
     """
-    Random integer.
+    The function generates random integer between two given values
+
+    Args:
+        min : Minimum Value : int
+        max : Maximum Value : int
+    Returns:
+        random integer : int
     """
-    return random.randint(min, max)
+    try:
+        value = random.randint(min, max) # Generate random integer using random function
+        return value
+    except Exception as e:
+        print("Error in integer generation!!") # Error Handling
+        print(e)
 
 
-def function_B():
-    return random.choice(['+', '-', '*'])
+def generateOperator():
+    """
+    The function randomly outputs arthimetic operator symbols
+
+    Args:
+        nil
+
+    Returns:
+        random arthimetic symbol : str
+    """
+    return random.choice(['+', '-', '*']) # random choice using random function
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def mathOperator(num1, num2, operator):
+    """
+    The function generates the math problem and the answer.
+
+    Args:
+        num1 : First number        : int 
+        num2 : Second number       : int 
+        operator  : Arithmetic operator : str 
+
+    Returns:
+        log_statement : arthimetic problem : str
+        a : return of arthimetic operation : int
+    """
+
+    log_statement = f"{num1} {operator} {num2}" # Problem statement string
+    
+    if operator == '+':
+        ans = num1 + num2 # Addition
+    elif operator == '-': 
+        ans = num1 - num2 # Subtraction
+    else: 
+        ans = num1 * num2 # Multiplication
+    return log_statement, ans
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
 
-    print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    try:
+            
+        s = 0
+        t_q = 3.14159265359
+        t_q =3
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        print("Welcome to the Math Quiz Game!")
+        print("You will be presented with math problems, and you need to provide the correct answers.")
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        for _ in range(t_q):  # Loop to determine the number of rounds in quiz
+            # Function call to generate numbers and operator
+            n1 = generateInteger(1, 10); 
+            n2 = generateInteger(1, 512); 
+            o = generateOperator() 
+            # Mathematical Operation 
+            PROBLEM, ANSWER = mathOperator(n1, n2, o) 
+            print(f"\nQuestion: {PROBLEM}")
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            if useranswer == ANSWER: # Check user input with actual operation
+                print("Correct! You earned a point.") 
+                s += 1
+            else:
+                print(f"Wrong answer. The correct answer is {ANSWER}.") 
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+        print(f"\nGame over! Your score is: {s}/{t_q}") # Print final Score
+
+    except TypeError as e:
+        print(f"Error has occurred on line {e.__traceback__.tb_lineno}") # Error Handling
+        print(e)
+
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,35 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generateInteger, generateOperator, mathOperator
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generateInteger(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generateInteger(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
+        print("      Test 1 - Passed")
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generateOperator(self):
+        for i in range(10):
+            self.assertTrue(generateOperator() in ['+', '-', '*'])
+        print("     Test 2 - Passed")
 
-    def test_function_C(self):
+    def test_mathOperator(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (10, 2, '*', '10 * 2', 20),
+                (4, 3, '-', '4 - 3', 1)
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                achieved_problem, achieved_answer = mathOperator(num1, num2, operator)
+                self.assertTrue(achieved_answer==expected_answer)
+                self.assertTrue(achieved_problem==expected_problem)
+            print("     Test 3 - Passed")  
 
 if __name__ == "__main__":
     unittest.main()
