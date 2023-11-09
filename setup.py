ae1fdbd27e767756b83eead9f10c1fbbd032554c
rahul-alpha <rahul.hirur@fau.de>
2023-11-09 20:21:03 +0100

    setup file edit 1

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'random'
+    ],
+    author='Rahul Hirur',
+    author_email='rahul.hirur@fau.de',
+    description='Submission of assignment number 2',
+    url='https://github.com/rahulhirur/DSSS_HW2.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
